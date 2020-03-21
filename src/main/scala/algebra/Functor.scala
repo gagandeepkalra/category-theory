9d318c3aabcfed8a4caec463a73a93b76ed3b641
Kalra <gagandeepkalra1994@gmail.com>
2020-03-21 09:30:09 +0800

    added functor algebra

---
+++ src/main/scala/algebra/Functor.scala
@@ -0,0 +1,7 @@
+package algebra
+
+import scala.language.higherKinds
+
+trait Functor[F[_]] {
+  def map[A, B](fa: F[A])(f: A => B): F[B]
+}
--- src/main/scala/free/FreeMonad.scala
+++ src/main/scala/free/FreeMonad.scala
@@ -1,5 +1,7 @@
 package free
 
+import algebra.Functor
+
 import scala.annotation.tailrec
 import scala.language.higherKinds
 
@@ -10,10 +12,6 @@ Something we can wrap around an arbitrary type constructor (a F[_]) to construct
 the structure of the computation from its interpreter, thereby allowing different interpretation depending on context.
  */
 object FreeMonad {
-
-  trait Functor[F[_]] {
-    def map[A, B](fa: F[A])(f: A => B): F[B]
-  }
 
   trait Monad[F[_]] extends Functor[F] {
     def flatMap[A, B](fa: F[A])(f: A => F[B]): F[B]
