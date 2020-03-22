ad51e1fec1523b688e2091b4dc6fbc42ce81e421
Kalra <gagandeepkalra1994@gmail.com>
2020-03-22 13:12:05 +0800

    added comonad algebra

---
+++ src/main/scala/algebra/CoMonad.scala
@@ -0,0 +1,9 @@
+package algebra
+
+trait CoMonad[F[_]] extends Functor[F] {
+  def extract[A](fa: F[A]): A
+
+  def coFlatten[A](fa: F[A]): F[F[A]]
+
+  def coFlatMap[A, B](fa: F[A])(f: F[A] => B): F[B] = map(coFlatten(fa))(f)
+}
