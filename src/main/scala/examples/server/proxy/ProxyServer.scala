0cd391594be68697c0024aeb0a66d937fa361861
Kalra <gagandeepkalra1994@gmail.com>
2020-03-27 09:07:28 +0800

    Added a proxy tcp server implementation

---
+++ src/main/scala/examples/server/proxy/ProxyServer.scala
@@ -0,0 +1,33 @@
+package examples.server.proxy
+
+import java.net.{ServerSocket, Socket}
+
+import examples.server.reader.InputReader
+
+class ProxyServer(address: String, port: Int, proxyPort: Int)
+    extends Thread
+    with InputReader {
+  val proxySocket = new ServerSocket(proxyPort)
+
+  override def run(): Unit = {
+    while (true) {
+      println("Proxy accepting connections...")
+      val connectionSocket = proxySocket.accept()
+      val connectionInputStream = connectionSocket.getInputStream
+      println(s"Proxy Accepted Connection: $connectionSocket")
+
+      val serverSocket = new Socket(address, port)
+      val serverInputStream = serverSocket.getInputStream
+
+      val message: String = readRecur(connectionInputStream)
+      println(s"Proxy server received: $message")
+      serverSocket.getOutputStream.write(message.getBytes("UTF-8"))
+      serverSocket.shutdownOutput()
+
+      val response = readRecur(serverInputStream)
+      println(s"Proxy server received $response from server")
+      connectionSocket.getOutputStream.write(response.getBytes("UTF-8"))
+      connectionSocket.shutdownOutput()
+    }
+  }
+}
---
+++ src/main/scala/examples/server/proxy/run/proxy.scala
@@ -0,0 +1,29 @@
+package examples.server.proxy.run
+
+import java.net.Socket
+
+import examples.server.proxy.{ProxyServer, SimpleServer}
+import examples.server.reader.InputReader
+
+object proxy extends InputReader {
+  def main(args: Array[String]): Unit = {
+    val server = new SimpleServer(2222)
+    println("starting server")
+    server.start()
+
+    val proxyServer = new ProxyServer("0.0.0.0", 2222, 3333)
+    println("starting proxy server")
+    proxyServer.start()
+
+    // Begin testing
+    val proxyServerSocket = new Socket("0.0.0.0", 2222)
+    val message: String = "Hello!"
+    println(s"Client sending message: $message")
+    proxyServerSocket.getOutputStream.write(message.getBytes("UTF-8"))
+    proxyServerSocket.shutdownOutput()
+
+    val response = readRecur(proxyServerSocket.getInputStream)
+    println(s"Client received: $response")
+  }
+
+}
